7639d4718b8f07480cd08ac606f2ed86c45c5fbc
Ingo Reschke <kontakt@ingo-reschke.de>
2012-09-16 21:39:41 +0200

    adding @Before Method to avoid duplicates

--- test/de/ingoreschke/redis/TestRedis.java
+++ test/de/ingoreschke/redis/TestRedis.java
@@ -1,19 +1,24 @@
 package de.ingoreschke.redis;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestRedis {
+	private IRedis redis;
+	
+	@Before	
+	public void setUp(){
+		redis = new MockRedis();
+	}
 	
 	@Test
 	public void testGet(){
-		IRedis redis = new MockRedis();
 		redis.get("test");
 	}
 	
 	@Test
 	public void testGet_keyDontExits() {
-		IRedis redis = new MockRedis();
 		Assert.assertEquals(null, redis.get("test"));
 	}
 	
@@ -22,20 +27,17 @@ public class TestRedis {
 	public void testGet_keyExists(){
 		String key = "testkey";
 		String expected = "test";
-		IRedis redis = new MockRedis(); 
 		redis.set(key, expected);
 		Assert.assertEquals(expected, redis.get(key));
 	}
 	
 	@Test
 	public void testSet(){
-		IRedis redis = new MockRedis();
 		redis.set("testKey","testValue");
 	}
 	
 	@Test
 	public void testSet_String(){
-		IRedis redis = new MockRedis();
 		String key = "testkey";
 		String expected = "testValue";
 		redis.set(key, expected);
@@ -46,7 +48,6 @@ public class TestRedis {
 	/* I should test when value is not a string.
 	@Test (expected )
 	public void testSet_NoString(){
-		IRedis redis = new MockRedis();
 		
 	}
 	*/
