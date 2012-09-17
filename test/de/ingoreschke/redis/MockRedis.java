5ea9563699fc0263a9315b6a6e04a8119c21a54a
Ingo Reschke <kontakt@ingo-reschke.de>
2012-09-17 22:17:33 +0200

    adding the implementation code for the string mockup

--- test/de/ingoreschke/redis/MockRedis.java
+++ test/de/ingoreschke/redis/MockRedis.java
@@ -1,16 +1,22 @@
 package de.ingoreschke.redis;
 
+import java.util.HashMap;
+
 public class MockRedis implements IRedis {
+	private HashMap<String, String> store;
+	
+	public MockRedis() {
+		store = new HashMap<String, String>();
+	}
 	
 	@Override
 	public String get(String key) {
-	
-		return null;
+		return store.get(key);
 	}
 	
 	@Override
 	public String set(String key, Object value) {
-		
+		store.put(key, value.toString());
 		return "OK";
 	}
 	
