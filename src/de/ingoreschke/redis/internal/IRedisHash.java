59ff56f002ba1d157af91c4c2326e2dccb536df5
Ingo Reschke <kontakt@ingo-reschke.de>
2012-10-28 14:47:38 +0100

    refactor a little bit.

--- src/de/ingoreschke/redis/internal/IRedisHash.java
+++ src/de/ingoreschke/redis/internal/IRedisHash.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface IRedisHash {
-	public String hGet(String key, String field);
+	String hGet(String key, String field);
 	
 	/**
 	 * 
@@ -14,7 +14,7 @@ public interface IRedisHash {
 	 * @return  1 if field is a new field in the hash and value was set. <br>
 	 * 			0 if field already exists in the hash and the value was updated.	
 	 */
-	public int hSet(String key, String field, String value);
+	int hSet(String key, String field, String value);
 	
 	/**
 	 * Returns the values associated with the specified fields in the hash stored at key.
@@ -22,7 +22,7 @@ public interface IRedisHash {
 	 * @param field
 	 * @return
 	 */
-	public List<String> hMGet(String key, String...fields);
+	List<String> hMGet(String key, String...fields);
 	
 	/**
 	 * Set multiple hash fields to multiple values
@@ -30,12 +30,12 @@ public interface IRedisHash {
 	 * @param map : field-value pairs
 	 * @return 
 	 */
-	public String hMSet(String key, Map<String, String> map);
+	String hMSet(String key, Map<String, String> map);
 	
 	/**
 	 *Returns all field names in the hash stored at key
 	 * @param key
 	 * @return
 	 */
-	public List<String> hKeys (String key);
+	List<String> hKeys (String key);
 }
--- src/de/ingoreschke/redis/internal/IRedisString.java
+++ src/de/ingoreschke/redis/internal/IRedisString.java
@@ -4,15 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 public interface IRedisString {
-	public String get(String key);
-	public String set(String key, String value);
-	public long incr(String key);
+	String get(String key);
+	String set(String key, String value);
+	long incr(String key);
 	/**
 	 * Get a List with all values of given keys
 	 * @param keys comma separated list of keystrings (i.e. "key1", "key2", "key3") 
 	 * @return List
 	 */
-	public List<String> mGet (String ... keys);
-	public String mSet(Map<String, String> map);
-	
+	List<String> mGet (String ... keys);
+	String mSet(Map<String, String> map);
 }
