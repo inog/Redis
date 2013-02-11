883accc884f2ae88a39b0b30d55c21cd911b6aa1
Ingo Reschke <kontakt@ingo-reschke.de>
2013-02-12 00:27:25 +0100

    remove not necessary code

--- test/de/ingoreschke/jedis/TestJedisChat.java
+++ test/de/ingoreschke/jedis/TestJedisChat.java
@@ -1,7 +1,6 @@
 package de.ingoreschke.jedis;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import de.ingoreschke.redis.TestRedisChat;
 
@@ -15,13 +14,4 @@ public class TestJedisChat extends TestRedisChat {
 		cut = new JedisChat();
 	}
 
-
-	@Test
-	public void testGetAllMesssages(){
-		String channel = "mySportChannel";
-		cut.publish(channel, "welcome to my Sport channel");
-		cut.publish(channel, "second message");
-		cut.publish(channel, "third msg");
-		cut.publish(channel, "oh this one is realy long.");
-	}
 }
